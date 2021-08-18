1b49e8344de87586d35fc3a0d1cde3b7797b3fc5
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-08-18 16:38:30 +0100

    Add disabled prop to ExternalLink

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -3,12 +3,18 @@ import styles from "../styles/ExternalLink.module.css"
 
 interface Props {
     children?: React.ReactNode
-    href: string
+    href?: string | null
+    disabled?: boolean
 }
 
 export default function ExternalLink(props: Props) {
     return (
-        <a className={styles.linkContainer} href={props.href}>
+        <a
+            className={`${styles.linkContainer} ${
+                props.disabled ? styles.disabled : ""
+            }`}
+            href={props.href ?? ""}
+        >
             {props.children}
         </a>
     )
